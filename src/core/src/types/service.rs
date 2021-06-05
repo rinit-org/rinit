35a23cda8bc0139885a766caaa8c24e5faca1d11
Danilo Spinella <oss@danyspin97.org>
2021-06-05 23:43:14 +0200

    core: Add helper methods in Service enum

--- src/core/src/types/service.rs
+++ src/core/src/types/service.rs
@@ -5,10 +5,31 @@ use serde::{
 
 use super::*;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub enum Service {
     Bundle(Bundle),
     Longrun(Longrun),
     Oneshot(Oneshot),
     Virtual(Virtual),
+}
+
+impl Service {
+    pub fn name(&self) -> &str {
+        match &self {
+            Self::Bundle(bundle) => &bundle.name,
+            Self::Longrun(longrun) => &longrun.name,
+            Self::Oneshot(oneshot) => &oneshot.name,
+            Self::Virtual(virtual_service) => &virtual_service.name,
+        }
+    }
+
+    pub fn dependencies(&self) -> &[String] {
+        match &self {
+            Self::Bundle(bundle) => &bundle.contents,
+            Self::Longrun(longrun) => &longrun.options.dependencies,
+            Self::Oneshot(oneshot) => &oneshot.options.dependencies,
+            // TODO: What should be done here?
+            Self::Virtual(_virtual_service) => &[],
+        }
+    }
 }
