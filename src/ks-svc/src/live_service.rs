d27630731ab77616e26921f54cf85f70dd99525c
Danilo Spinella <oss@danyspin97.org>
2021-11-21 19:37:44 +0100

    svc: Fully implement services start

--- src/ks-svc/src/live_service.rs
+++ src/ks-svc/src/live_service.rs
@@ -1,12 +1,15 @@
-use anyhow::Result;
 use async_pidfd::PidFd;
+use async_std::sync::{
+    Condvar,
+    Mutex,
+};
 use chrono::prelude::*;
 use kansei_core::{
     graph::Node,
     types::ScriptConfig,
 };
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq)]
 pub enum ServiceStatus {
     Reset,
     Up,
@@ -18,8 +21,9 @@ pub enum ServiceStatus {
 pub struct LiveService {
     pub node: Node,
     pub updated_node: Option<Node>,
-    pub status: ServiceStatus,
+    pub status: Mutex<ServiceStatus>,
     pub status_changed: Option<DateTime<Local>>,
+    pub wait: Condvar,
     // Skip starting and stopping values here
     pub last_status: Option<ServiceStatus>,
     // first element for Oneshot::start and Longrun::run
@@ -35,8 +39,9 @@ impl LiveService {
         Self {
             node,
             updated_node: None,
-            status: ServiceStatus::Reset,
+            status: Mutex::new(ServiceStatus::Reset),
             status_changed: None,
+            wait: Condvar::new(),
             last_status: None,
             config: None,
             environment: None,
@@ -45,18 +50,30 @@ impl LiveService {
         }
     }
 
-    pub fn change_status(
+    pub async fn change_status(
         &mut self,
         new_status: ServiceStatus,
     ) {
-        match self.status {
+        let mut status = self.status.lock().await;
+        match *status {
             ServiceStatus::Starting => {}
             ServiceStatus::Stopping => {}
             _ => {
-                self.last_status = Some(self.status.clone());
+                self.last_status = Some(status.clone());
             }
         }
-        self.status = new_status;
+        *status = new_status;
         self.status_changed = Some(chrono::offset::Local::now());
+        self.wait.notify_all();
+    }
+
+    pub async fn wait_on_status(&self) -> ServiceStatus {
+        (*self
+            .wait
+            .wait_until(self.status.lock().await, |status| {
+                *status == ServiceStatus::Up || *status == ServiceStatus::Down
+            })
+            .await)
+            .clone()
     }
 }
--- src/ks-svc/src/live_service_graph.rs
+++ src/ks-svc/src/live_service_graph.rs
@@ -1,12 +1,15 @@
 use std::collections::HashMap;
 
 use anyhow::Result;
+use async_recursion::async_recursion;
 use async_std::{
-    prelude::*,
-    stream,
-    stream::StreamExt,
+    process::{
+        Command,
+        Stdio,
+    },
     sync::{
         RwLock,
+        RwLockUpgradableReadGuard,
         RwLockWriteGuard,
     },
 };
@@ -43,18 +46,69 @@ impl LiveServiceGraph {
     }
 
     pub async fn start_all_services(&self) {
-        stream::from_iter(self.live_services.read().await.iter()).map(async move |live_service| {
-            if live_service.read().await.node.service.should_start() {
-                self.start_service(&mut live_service.write().await).await;
-            }
-        });
+        let services = self.live_services.read().await;
+        let futures: Vec<_> = services
+            .iter()
+            .map(async move |live_service| {
+                if live_service.read().await.node.service.should_start() {
+                    self.start_service_impl(&mut live_service.write().await)
+                        .await;
+                }
+            })
+            .collect();
+        for future in futures {
+            future.await;
+        }
     }
 
+    #[async_recursion]
     async fn start_service(
         &self,
         live_service: &mut RwLockWriteGuard<'_, LiveService>,
     ) {
-        live_service.change_status(ServiceStatus::Up);
+        live_service.change_status(ServiceStatus::Starting).await;
+        self.start_dependencies(live_service).await;
+        self.start_service_impl(live_service).await;
+    }
+
+    async fn start_dependencies(
+        &self,
+        live_service: &mut RwLockWriteGuard<'_, LiveService>,
+    ) {
+        // Start dependencies
+        let futures: Vec<_> = live_service
+            .node
+            .service
+            .dependencies()
+            .iter()
+            .map(async move |dep| {
+                let services = self.live_services.read().await;
+                let dep_service = services
+                    .get(*self.indexes.get(dep).expect("This should nevel happen"))
+                    .unwrap()
+                    .upgradable_read()
+                    .await;
+                if {
+                    let status = dep_service.status.lock().await;
+                    *status != ServiceStatus::Up
+                        && *status != ServiceStatus::Starting
+                        && *status != ServiceStatus::Stopping
+                } {
+                    self.start_service(&mut RwLockUpgradableReadGuard::upgrade(dep_service).await)
+                        .await;
+                }
+            })
+            .collect();
+        for future in futures {
+            future.await;
+        }
+    }
+
+    async fn start_service_impl(
+        &self,
+        live_service: &mut RwLockWriteGuard<'_, LiveService>,
+    ) {
+        self.wait_on_deps(&*live_service).await;
         let exe = match &live_service.node.service {
             Service::Oneshot(_) => Some("ks-run-oneshot"),
             Service::Longrun(_) => Some("ks-run-longrun"),
@@ -62,10 +116,37 @@ impl LiveServiceGraph {
             Service::Virtual(_) => None,
         };
         if let Some(exe) = exe {
+            // TODO: Add logging and remove unwrap
             Command::new(exe)
                 .stdin(Stdio::null())
                 .stdout(Stdio::inherit())
-                .spawn();
+                .spawn()
+                .unwrap();
+        }
+    }
+
+    async fn wait_on_deps(
+        &self,
+        live_service: &LiveService,
+    ) {
+        let futures: Vec<_> = live_service
+            .node
+            .service
+            .dependencies()
+            .iter()
+            .map(async move |dep| {
+                let services = self.live_services.read().await;
+                let dep_service = services
+                    .get(*self.indexes.get(dep).expect("This should nevel happen"))
+                    .unwrap()
+                    .read()
+                    .await;
+                dep_service.wait_on_status().await.clone()
+            })
+            .collect();
+
+        for future in futures {
+            future.await;
         }
     }
 }
