030636f73b41e9cdd2f81058d1708bc0b4ba76fd
Danilo Spinella <oss@danyspin97.org>
2022-03-13 11:08:14 +0100

    message: Update host address

--- src/message/src/get_host_address.rs
+++ src/message/src/get_host_address.rs
@@ -3,9 +3,9 @@ use nix::unistd::Uid;
 lazy_static! {
     static ref UID: Uid = Uid::current();
     static ref HOST: String = if UID.is_root() {
-        "/run/ks/.socket".to_string()
+        "/run/kansei/.socket".to_string()
     } else {
-        format!("/run/user/{}/ks/.socket", UID.as_raw())
+        format!("/run/user/{}/kansei/.socket", UID.as_raw())
     };
 }
 
