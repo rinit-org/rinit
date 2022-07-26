397bead0b739b731df6de5e95d7e0e1c4721fd8a
Danilo Spinella <danilo.spinella@suse.com>
2022-07-26 13:55:54 +0200

    bundle_options: Optimize serialization

--- service/src/types/bundle_options.rs
+++ service/src/types/bundle_options.rs
@@ -8,6 +8,7 @@ use super::runlevel::RunLevel;
 /// Store options for Longrun and Oneshot
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub struct BundleOptions {
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
     pub contents: Vec<String>,
     #[serde(default, skip_serializing_if = "RunLevel::is_default")]
     pub runlevel: RunLevel,
