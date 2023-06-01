eb48caf3ab5928ab3ecfc0ee30752d4fba4f7951
Danilo Spinella <oss@danyspin97.org>
2023-06-01 18:53:50 +0200

    Remove unused source files

--- svc/src/supervision/mod.rs
+++ svc/src/supervision/mod.rs
@@ -4,14 +4,7 @@ mod kill_process;
 pub use kill_process::kill_process;
 mod log_stdio;
 pub use log_stdio::log_output;
-mod pidfd_send_signal;
-pub use pidfd_send_signal::pidfd_send_signal;
 mod run_short_lived_script;
 pub use run_short_lived_script::run_short_lived_script;
-mod signal_wait;
-pub use signal_wait::{
-    signal_wait,
-    signal_wait_fun,
-};
 mod supervisor;
 pub use supervisor::Supervisor;
